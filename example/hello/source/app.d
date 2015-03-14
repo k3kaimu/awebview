689b873acb3f5ce2c6852f79a656dae4f6a2b2a4
k3_kaimu <enjouzensyou.boinc@gmail.com>
2015-03-15 05:07:30 +0900

    add an example 'hello'

---
+++ example/hello/source/app.d
@@ -0,0 +1,28 @@
+import awebview.gui.application;
+import awebview.gui.activity;
+import awebview.gui.html;
+import awebview.wrapper;
+
+void main()
+{
+    auto app = SDLApplication.instance;
+    auto pref = WebPreferences.recommended;
+    app.createActivity(pref, delegate(WebSession session){
+
+      // MainActivityというIDのActivityを作成
+      auto activity = new SDLActivity("MainActivity", 600, 400, "Hello!", session);
+      
+      // hello.htmlを読み込んで、helloというIDのページを作成
+      auto helloPage = new TemplateHTMLPage!(import("hello.html"))("hello", null);
+      
+      // Activityにページを登録
+      activity ~= helloPage;
+      
+      // IDがhelloのページの読み込み
+      activity.load("hello");
+      return activity;
+    });
+    
+    // アプリケーションを走らせる
+    app.run();
+}
