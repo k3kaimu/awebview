8be9a0f812a08e14edda4a33d47513c20c8f315d
k3_kaimu <enjouzensyou.boinc@gmail.com>
2015-03-25 09:46:26 +0900

    remove some noisy comment

--- example/multiview/source/app.d
+++ example/multiview/source/app.d
@@ -8,23 +8,17 @@ void main()
 {
     auto app = SDLApplication.instance;
     auto pref = WebPreferences.recommended;
-    app.createActivity(pref, delegate(WebSession session){
 
-      // MainActivityというIDのActivityを作成
+    app.createActivity(pref, delegate(WebSession session){
       auto activity = new SDLActivity("MainActivity", 600, 400, "Hello!", session);
-      
-      // hello.htmlを読み込んで、helloというIDのページを作成
       auto topPage = new TopPage();
-      
-      // Activityにページを登録
+
       activity ~= topPage;
-      
-      // IDがhelloのページの読み込み
       activity.load("topPage");
+
       return activity;
     });
-    
-    // アプリケーションを走らせる
+
     app.run();
 }
 
@@ -46,21 +40,17 @@ final class TopPage : TemplateHTMLPage!(import("top.html"))
 
     void onClickOpenWindow(FiredContext ctx, WeakRef!(const(JSArrayCpp)) args)
     {
-        application.to!SDLApplication.createActivity(WebPreferences.recommended, delegate(WebSession session){
-            // MainActivityというIDのActivityを作成
+        application.to!SDLApplication.createActivity(WebPreferences.recommended,
+        delegate(WebSession session){
             ++_n;
             string strN = _n.to!string;
 
             auto activity = new SDLActivity("MainActivity" ~ strN, 600, 400, strN ~ "!", session);
-
-            // hello.htmlを読み込んで、helloというIDのページを作成
             auto helloPage = new TemplateHTMLPage!(import("hello.html"))("hello", null);
 
-            // Activityにページを登録
             activity ~= helloPage;
-
-            // IDがhello%[N%]のページの読み込み
             activity.load("hello");
+
             return activity;
         });
     }
